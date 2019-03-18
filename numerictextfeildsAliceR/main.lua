e81e97b6ce606f6be848f2eb5501223fd8c954db
Alice Roberts <alice.roberts@ocsbstudent.ca>
2019-03-18 09:28:54 -0400

    added numeric text feilds

---
+++ numerictextfeildsAliceR/main.lua
@@ -0,0 +1,37 @@
+-- Title: NumericTextFields
+-- Name: Alice R
+-- Course: ICS2O
+-- This program asks the user to answer a random math question.
+------------------------------------------------------------------------
+
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- sets the background colour
+display.setDefault("background", 255/255, 255/255, 153/255) 
+
+-----------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------
+
+-- create local variables
+local questionObject
+local correctObject
+local numericTextFields
+local randomNumber1
+local randomNumber2
+local userAnswer
+local correctAnswer
+
+-----------------------------------------------------------------------
+-- LOCALO FUNCTIONS
+-----------------------------------------------------------------------
+
+local function AskQuestion()
+	-- generate 2  random numbers beetween a max. and a min. number
+	randomNumber1 = math.random(0,10)
+	randomNumber2 = math.random(0,10)
+
+	correctAnswer = randomNumber1 + randomNumber2
+
+	-- create question
