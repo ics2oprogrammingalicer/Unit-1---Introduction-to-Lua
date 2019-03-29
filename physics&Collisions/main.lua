1adb72b71d8cf789be1f77576255e54575bb3838
Alice Roberts <47784287+s436512@users.noreply.github.com>
2019-03-29 09:14:15 -0400

    added stuff

--- physics&Collisions/main.lua
+++ physics&Collisions/main.lua
@@ -97,23 +97,41 @@ local function secondBall()
 	local ball2 = display.newImage("images/super_ball.png", 0, 0)
 
 	-- add to physics
-	physics.addBody(ball2, {density=1.0, friction=0.5, bounce=0.9, radius=25})
+	physics.addBody(ball2, {density=1.0, friction=0.5, bounce=0.9, radius=65})
+
+  -- make the ball bigger than the original size
+  ball2:scale(3, 3)
 end
 
 -------------------------------------------------------------------------
 -- create the third ball
 local function thirdBall()
-	-- creating second ball
+	-- creating third ball
 	local ball3 = display.newImage("images/super_ball.png", 0, 0)
 
 	-- add to physics
-	physics.addBody(ball3, {density=1.0, friction=0.5, bounce=0.9, radius=50})
+	physics.addBody(ball3, {density=1.0, friction=0.5, bounce=0.9, radius=0.7})
+
+  -- make ball smaller than original size
+  ball3:scale(0.5, 0.5) 
 end
 
+-------------------------------------------------------------------------
+-- create the fourth ball
+local function fourthBall()
+  -- creating second ball
+  local ball4 = display.newImage("images/super_ball.png", 0, 0)
 
+  -- add to physics
+  physics.addBody(ball4, {density=1.0, friction=0.5, bounce=0.9, radius=200})
+
+  -- make ball smaller than original size
+  ball4:scale(10, 10) 
+end
 -------------------------------------------------------------------------
 -- TIMER DELAYS 
 -------------------------------------------------------------------------
 timer.performWithDelay( 0, firstBall)
 timer.performWithDelay( 500, secondBall)
-timer. performWithDelay(1000, thirdBall)
+timer.performWithDelay(1000, thirdBall)
+timer.performWithDelay(1500, fourthBall)
