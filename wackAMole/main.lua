8976019426b6bcfddc72d172f9932a9c9e5ae7ee
Alice Roberts <47784287+s436512@users.noreply.github.com>
2019-04-05 09:45:02 -0400

    added stuff

---
+++ wackAMole/main.lua
@@ -0,0 +1,88 @@
+-- Title: WhackAMole
+-- Name: ALice R
+-- Course: ICS2O/3C
+-- This program places a random object on the screen. if the user clicks on it in time ,
+-- the score increases by 1.
+
+display.setStatusBar(display.HiddenStatusBar)
+
+------------------------------------------------------------------------------
+--LOCAL VARIABLES AND OBJECTS
+------------------------------------------------------------------------------
+-- Creating a Background
+local bkg = display.newRect( 0, 0, display.contentWidth, display.contentHeight )
+
+    -- set background Colour
+    display.setDefault("background", 153/255, 0/255, 26/255)
+
+    -- setting position
+    bkg.anchorX = 0
+    bkg.anchorY = 0
+    bkg.x = 0
+    bkg.y = 0
+
+    bkg.isVisible = true
+
+-- creating Mole
+local mole = display.newImage( "Images/mole.png", 0, 0 )
+
+    -- setting position
+    mole.x = display.contentCenterX
+    mole.y = display.contentCenterY
+
+    --scale the mole to original size
+    mole:scale(0.3, 0.3)
+
+    -- make the mole invisible
+    mole.isVisible = false
+
+-- create variable to keep track of score
+local score 
+local scoreObject = display.newText("", display.contentWidth/5, display.contentHeight/3, nil, 50)
+scoreObject:setTextColor(0/255, 230/255, 38/255)
+
+----------------------------------------------------------------------------
+--FUNCTIONS
+----------------------------------------------------------------------------
+-- this function makes the mole appesr in a random(x,y) position on the screen
+-- before calling the hide function
+function PopUp( ) 
+
+	-- choose a random position on the screen between 0 and the size of the screen
+	mole.x = math.random( 0, display.contentWidth )
+	mole.y = math.random( 0, display.contentHeight )
+
+	-- make the mole visible 
+	mole.isVisible = true
+
+	-- call hide after 500 milliseconds
+	timer.performWithDelay( 500, hide )
+end
+
+-- this function calls the popUp after 3 sceonds
+function PopUpDelay( )
+	timer.performWithDelay( 3000, PopUp )
+end
+
+-- this function makes the  mole invisible and then calls the PopUpDelay function
+function Hide( )
+
+	-- changing visibility
+	mole.isVisible = false
+
+	-- call PopUpDelay function
+	timer.performWithDelay( 0, PopUpDelay )
+end
+
+-- this function starts the game
+function GameStart( )
+	PopUpDelay()
+end
+
+-- this function increments the score only if the mole is clicked. it then displays
+-- the new Score.
+function Whacked( )
+
+	-- if touch phase just started
+	if (event.phase == "began") then
+		
